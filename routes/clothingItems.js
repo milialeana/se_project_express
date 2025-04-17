613e3220c0d2fb7c23e990cc122cbcfc978dba9a
Kate <k.wollet@outlook.com>
2025-04-17 13:35:50 -0400

    Fix auth middleware

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -2,14 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 const {
-  getItems,
   createClothingItem,
   deleteItem,
   likeItem,
   dislikeItem,
 } = require("../controllers/clothingItems");
 
-router.get("/", getItems);
 router.post("/", createClothingItem);
 router.delete("/:itemId", deleteItem);
 router.put("/:itemId/likes", likeItem);
